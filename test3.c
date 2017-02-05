14592eaaa75a3c3f5d5c26d9c12cc81d69f066a9
Steven Reust <slreust@shockers.wichita.edu>
2017-02-05 00:46:33 +0000

    Update test3.c

--- test3.c
+++ test3.c
@@ -1,15 +1,9 @@
 #include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-printf("Begin\n");    
-
-printf("Mid\n");    
-PlaySound("C:\\Windows\\Media\\Windows Logoff Sound.wav", NULL, SND_ASYNC);
 PlaySound("C:\\Windows\\Media\\Ring05.wav", NULL, SND_LOOP | SND_ASYNC);
-printf("End\n");
 getchar(); 
 return 0;
 }
